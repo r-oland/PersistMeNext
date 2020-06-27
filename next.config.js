0807affdb8d92ee7467a0966d8f0f482c5ddb5fa
Roland Branten <roland.branten@gmail.com>
2020-06-27 23:40:06 +0200

    production test

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    skipWaiting: false,
-    register: false,
     disable: process.env.NODE_ENV !== "production",
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,8 @@
 import { AppProps } from "next/app";
-import { useEffect } from "react";
 import UserContextComp from "../firebase/useUser";
 import AppWrapper from "../global-components/AppWrapper";
 
-export {};
-declare global {
-  interface Window {
-    workbox: any;
-  }
-}
-
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // This hook only run once in browser after the component is rendered for the first time.
-  // It has same effect as the old componentDidMount lifecycle callback.
-  useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      "serviceWorker" in navigator &&
-      window.workbox !== undefined
-    ) {
-      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
-      // NOTE: set skipWaiting to false in next.config.js pwa object
-      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
-      window.workbox.addEventListener("waiting", () => {
-        if (
-          confirm(
-            "A new version is installed, reload to use the new version immediately?"
-          )
-        ) {
-          window.workbox.addEventListener("controlling", () => {
-            window.location.reload();
-          });
-          window.workbox.messageSW({ type: "SKIP_WAITING" });
-        } else {
-          // User rejected, new verion will be automatically load when user open the app next time.
-        }
-      });
-
-      window.workbox.register();
-    }
-  }, []);
-
   return (
     <UserContextComp>
       <AppWrapper>
