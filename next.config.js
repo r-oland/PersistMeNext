4fc7e1c2b52e67b5545f111d79a2ebef0c72c83a
Roland Branten <roland.branten@gmail.com>
2020-06-27 21:19:11 +0200

    sw update test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    skipWaiting: false,
     disable: process.env.NODE_ENV !== "production",
   },
 });
