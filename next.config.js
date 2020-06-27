8f4e586f7b7bfcaea8bd9e914d7cd82c692ef15a
Roland Branten <roland.branten@gmail.com>
2020-06-27 13:44:30 +0200

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
+});
+
 module.exports = withBundleAnalyzer({});
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,10 @@ import Document, {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
+const APP_NAME: string = "PersitMe";
+const APP_DESCRIPTION: string =
+  "A time management app for the activities that matter";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -37,7 +41,31 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FDC61A" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
